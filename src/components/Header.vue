b9b55281a884473bc5955ef19cec69b85382ebb7
A.W. Kritikos <anton.kritikos@hva.nl>
2021-02-19 15:58:12 +0100

    Added header with MD tabs and implemented routing path for FAQ

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <md-tabs id="nav" md-sync-route>
+    <md-tab  v-for="tab in tabs" :key="tab.title" :md-label="tab.title" :to="tab.route" :exact="tab.exact">
+    </md-tab>
+  </md-tabs>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { MdButton, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdTabs)
+
+@Component
+export default class HeaderComponent extends Vue {
+  data () {
+    return {
+      tabs: [
+        { title: 'Home', route: '/', exact: true },
+        { title: 'About', route: '/about' },
+        { title: 'F.A.Q', route: '/faq' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  #nav
+    a
+      font-weight: bold
+      color: #2c3e50
+      &.router-link-exact-active
+        color: #75BD84
+
+    .md-tabs-indicator
+      background-color: #75BD84
+</style>
---
+++ src/views/FAQ.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="WODEH BROER IS FAQ"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class FAQ extends Vue {}
+</script>
