ff03083ac23c912a0fd1e10116ca084dd036a4c5
A.R Leurs <59964230+leursa@users.noreply.github.com>
2021-03-02 23:04:01 +0100

    Start of styling of the searchbar
    Done:
    Style of the searchbar itself
    Added a icon and the two inputs for the measurements
    
    Problems:
    Styling the volume input
    Getting the icon at the same length as the ingredient

--- src/components/search/searchbar.vue
+++ src/components/search/searchbar.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="searchbar">
+    <div class="searchbar-style md-elevation-3">
     <input type="text" class="search-input" placeholder="Search" v-model="ingredientName"  @change="getIngredients">
+      <md-icon>visibility</md-icon>
+    </div>
     <div class="search-results" v-if="ingredients.length || selectedIngredients.length">
       <div class="search-results-ingredients-selected-tab result-tab">
         <h3>Currently selected ingredients:</h3>
         <div class="search-results-ingredients-selected" v-for="(selectedIngredient, index) in selectedIngredients" :key="selectedIngredient.id">
-          <div class="search-result-selected-ingredient item" v-on:click="removeSelectedIngredient(index)">{{selectedIngredient.name}}</div>
+          <div class="search-result-selected-ingredient item" >{{selectedIngredient.name}}
+            <input type="number" id="quantity" class="quantity-ingredient" value="0">
+            <select id="volume-ingredient" name="volume">
+              <option id="grams" value="gram">gram</option>
+              <option value="milliliters">milliliters</option>
+            </select>
+            <div v-on:click="removeSelectedIngredient(index)">
+              <md-icon>clear</md-icon>
+            </div>
+          </div>
         </div>
       </div>
       <div class="search-results-ingredients-search-tab result-tab">
         <h3>Ingredients:</h3>
         <div class="search-results-ingredients-search" v-for="(ingredient, index) in ingredients" :key="ingredient.id">
-          <div class="search-result-ingredient-search item" v-on:click="setSelectedIngredient(index)">{{ingredient.name}}</div>
+            <div class="search-result-ingredient-search item" v-on:click="setSelectedIngredient(index)">{{ingredient.name}}</div>
         </div>
       </div>
     </div>
@@ -63,14 +75,33 @@ export default {
 </script>
 
 <style scoped lang="sass">
+  .quantity-ingredient
+    -webkit-appearance: none
+    -moz-appearance: textfield
+    border: none
+    width: 75px
+    margin: 0px 5px 0px 3px
+    background: #E9E9E9
+
+  .volume-ingredient
+    background: #E9E9E9
+    border: none
+    width: 100px
+
   .searchbar
     position: relative
     width: 600px
     margin: 0 auto
 
+    .searchbar-style
+      display: -ms-flexbox
+      display: flex
+      margin-bottom: 15px
+      background-color: #E9E9E9
+
     .search-input
       border: none
-      background-color: #F4F4F4
+      background-color: #E9E9E9
       font-size: 20px
       padding: 5px 10px
       width: 100%
@@ -83,9 +114,12 @@ export default {
       text-align: left
       padding: 10px 0
       box-shadow: 0 0 4px #000
+      font-size: 17px
 
       .result-tab
         border-bottom: 1px solid #000
+        padding-bottom: 5px
+        padding-top: 2px
 
         &:last-of-type
           border-bottom: 0
@@ -95,7 +129,7 @@ export default {
 
         .item
           background-color: rgba(83,83,83, 0.16)
-          padding: 10px
+          padding: 5px 10px 5px 10px
           margin: 5px
 
         .search-results-ingredients-selected
