3c9b2866fed8fe36fe063772f06ed6a494bec69c
StijnLimonard <70508897+StijnLimonard@users.noreply.github.com>
2021-03-02 19:49:41 +0100

    improved wysiwyg menubar
    improved wysiwyg menubar and added buttons, removed unused wysiwyg  features

--- src/components/CreateRecipe.vue
+++ src/components/CreateRecipe.vue
@@ -7,30 +7,44 @@
                 <label>Title</label>
                 <md-input v-model="type"></md-input>
             </md-field>
+            <br/>
             <searchbar/>
             <br/>
             <div class="editor">
-                <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-                    <div>
-                        <md-button class="md-icon-button" :md-ripple="false" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+                <editor-menu-bar :editor="editor" v-slot="{ commands }">
+                    <div class="menubar md-elevation-1">
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.bold">
                             <md-icon class="icon">format_bold</md-icon>
                         </md-button>
 
-                        <md-button class="md-icon-button" :md-ripple="false" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+                        <md-button class="md-icon-button md-elevation-1" :md-ripple="false" @click="commands.italic">
                             <md-icon class="icon">format_italic</md-icon>
                         </md-button>
 
-                        <md-button class="md-icon-button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike">
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.strike">
                             <md-icon class="icon">strikethrough_s</md-icon>
                         </md-button>
 
-                        <md-button class="md-icon-button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline">
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.underline">
                             <md-icon class="icon">format_underlined</md-icon>
                         </md-button>
 
-                        <md-button class="md-icon-button" :class="{ 'is-active': isActive.bullet_list() }" @click="commands.bullet_list">
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.bullet_list">
                             <md-icon class="icon">format_list_bulleted</md-icon>
                         </md-button>
+
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.ordered_list">
+                            <md-icon class="icon">format_list_numbered</md-icon>
+                        </md-button>
+
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.paragraph">
+                            <md-icon class="icon">format_clear</md-icon>
+                        </md-button>
+
+                        <md-button class="md-icon-button" :md-ripple="false" @click="commands.heading({ level: 2 })">
+                            <md-icon class="icon">format_size</md-icon>
+                        </md-button>
+
                     </div>
                 </editor-menu-bar>
                 <editor-content class="editor_content md-elevation-3" :editor="editor" />
@@ -39,6 +53,9 @@
                 <label>Tags</label>
                 <md-input v-model="type"></md-input>
             </md-field>
+            <br>
+            <md-button class="button md-elevation-3" v-on:click="saveAndPublish">Publish</md-button>
+            <md-button class="button md-elevation-3" v-on:click="save">Save for later</md-button>
         </div>
     </div>
 </template>
@@ -47,19 +64,14 @@
 import { Component, Vue } from 'vue-property-decorator'
 import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
 import {
-  Blockquote,
-  HardBreak,
   Heading,
-  HorizontalRule,
   OrderedList,
   BulletList,
   ListItem,
   Bold,
-  Code,
   Italic,
   Strike,
-  Underline,
-  History
+  Underline
 } from 'tiptap-extensions'
 import searchbar from '@/components/search/searchbar.vue'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
@@ -80,22 +92,25 @@ export default class CreateRecipeComponent extends Vue {
       editor: new Editor({
         content: '<p>add desription</p>',
         extensions: [
-          new Blockquote(),
           new BulletList(),
-          new HardBreak(),
-          new Heading({ levels: [1, 2, 3] }),
-          new HorizontalRule(),
+          new Heading({ levels: [2] }),
           new ListItem(),
           new OrderedList(),
           new Bold(),
-          new Code(),
           new Italic(),
           new Strike(),
-          new Underline(),
-          new History()
+          new Underline()
         ]
       })
     }
+  }
+
+  save () {
+    alert('dit doet nog niks')
+  }
+
+  saveAndPublish () {
+    this.save()
   }
 
   beforeDestroy () {
@@ -108,6 +123,7 @@ export default class CreateRecipeComponent extends Vue {
 <style scoped lang="sass">
     .divider
         width: 75%
+        opacity: 20%
     .inputdiv
         width: 60%
         margin: auto
@@ -120,18 +136,27 @@ export default class CreateRecipeComponent extends Vue {
             margin: auto
             margin-bottom: 5px
 
+            .is-active
+                background-color:#000000 50%
+                border: 1px solid black
+
+        .button
+            background-color: #45C760
+            opacity: 90%
+
         .editor
             position: relative
             text-align: left
             text-indent: 8px
-            width: 75%
+            width: 50%
             margin: auto
+            padding: 5px
             display: box
             box-sizing: border-box
+            .menubar
+                padding: 4px
 
             .editor_content
-                min-height: 10em
-                overflow-wrap: break-word
-                word-wrap: break-word
-                word-break: break-word
+                background-color: white
+                min-height: 200px
 </style>
