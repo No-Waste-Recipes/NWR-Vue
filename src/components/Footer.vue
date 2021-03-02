75a299d8070340f752716c95421c6aa6a511e363
StijnLimonard <70508897+StijnLimonard@users.noreply.github.com>
2021-03-02 19:54:02 +0100

    improved sass layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -66,31 +66,31 @@ export default class FooterComponent extends Vue {
         width: 100%
         background: #F6F4F5
 
-    .md-layout
-        width: 70%
-        margin: auto
+        .md-layout
+            width: 70%
+            margin: auto
 
-        .md-layout-item
-            background: #F6F4F5
-            text-align: center
+            .md-layout-item
+                background: #F6F4F5
+                text-align: center
 
-        .left
-        text-align: left
-        margin: 15px
+                ul
+                list-style: none
 
-    ul
-        list-style: none
+                .left
+                text-align: left
+                margin: 15px
 
-    .md-list
-        background-color: #F6F4F5
+                .md-list
+                    background-color: #F6F4F5
 
-        .md-list-item
-            text-align: center
-            display: block
-            vertical-align: top
+                    .md-list-item
+                        text-align: center
+                        display: block
+                        vertical-align: top
 
-    .seperatorLine
-        width: 3px
-        border-left: 3px solid #75BD84
+            .seperatorLine
+                width: 3px
+                border-left: 3px solid #75BD84
 
 </style>
