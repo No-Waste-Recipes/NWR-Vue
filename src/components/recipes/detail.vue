7dcfbd8c31c043d829798a26f486b78bcde69345
joep <joepboekhold@gmail.com>
2021-03-13 20:33:01 +0100

    Added comments and detail page for recipe

--- src/components/recipes/detail.vue
+++ src/components/recipes/detail.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="recipe">
-    <h1>{{recipe.title}}</h1>
-    <p>{{recipe.description}}</p>
-    <p>Ingredients</p>
-    <div v-for="ingredient in recipe.ingredient" :key="ingredient.id">
-      <p>{{ingredient.ingredients.name}}</p>
+    <div class="block top">
+      <div class="title">
+        <h1>{{recipe.title}}</h1>
+      </div>
+      <div class="author">
+        <p v-if="recipe.user">Gemaakt door: <span class="author-name">{{ recipe.user.username }}</span></p>
+      </div>
     </div>
-    <p>Gemaakt door:</p>
-    <p v-if="recipe.user">{{ recipe.user.username }}</p>
+    <div class="block image" style="background-image: url('https://www.leukerecepten.nl/wp-content/uploads/2020/10/basis-recept-wafels.jpg')">
+    </div>
+    <div class="block middle">
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item description">
+          <h2>Description</h2>
+          <p>{{recipe.description}}</p>
+        </div>
+        <div class="md-layout-item md-size-30">
+          <div class="ingredients-block md-elevation-3">
+            <h3>Ingredienten</h3>
+            <ul v-for="ingredient in recipe.ingredient" :key="ingredient.id" class="ingredients">
+              <li class="ingredient">{{ingredient.ingredients.name}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="block bottom">
+      <div class="comments">
+        <h3>Comments</h3>
+        <div v-for="comment in recipe.comments" :key="comment.id" class="comment">
+          <div class="user">
+            {{comment.user.username}}
+          </div>
+          <div class="content">
+            {{comment.text}}
+          </div>
+        </div>
+      </div>
+      <div v-if="loggedIn" class="place-comment">
+        <md-field>
+          <label>Plaats comment</label>
+          <md-textarea v-model="commentText"></md-textarea>
+        </md-field>
+        <button v-on:click="placeComment">Submit</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,7 +56,9 @@ export default {
   name: 'detail',
   data () {
     return {
-      recipe: Object
+      recipe: Object,
+      commentText: '',
+      loggedIn: this.$store.getters.isLoggedIn
     }
   },
   created () {
@@ -33,11 +73,89 @@ export default {
             this.$set(this, 'recipe', event.result)
           }
         )
+    },
+    async placeComment () {
+      const data = {
+        text: this.commentText
+      }
+      RecipeService.placeComment(this.$route.params.slug, data, this.$store.state.token)
+        .then(
+          event => {
+            this.recipe.comments.push({
+              id: event.comment.id,
+              recipeId: event.comment.recipeId,
+              text: event.comment.text,
+              user: {
+                username: event.comment.user.username
+              },
+              userId: event.comment.userId
+            })
+          }
+        )
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+  .recipe
+    text-align: left
+
+    .block
+      background-color: #FFF
+      padding: 40px
+
+      .comments
+        .comment
+          padding: 20px
+          border: 3px solid #F6F4F5
+          margin: 0px -30px 20px -30px
+
+          .user
+            font-weight: bold
+            font-size: 20px
+            border-bottom: 3px solid #75BD84
+            display: inline-block
+            line-height: 1.3
+
+          .content
+            margin-top: 10px
+
+      &.image
+        height: 400px
+        background-position: center
+        border-radius: 25px
+        margin: -40px -20px 20px -20px
+        background-size: cover
+
+      &.top
+        border-radius: 0 0 25px 25px
+        margin: 0 0 20px 0
+        padding-top: 10px
+
+      &.middle
+
+      .ingredients-block
+        padding: 5px 20px
+        background-color: #F6F4F5
+        border-radius: 20px
+
+        h3
+          border-bottom: 3px solid #75BD84
+          line-height: 2
+
+        .ingredients
+          list-style-type: none
+          padding: 0
+
+    .title
+      h1
+        font-size: 40px
+        border-bottom: 3px solid #75BD84
+        line-height: 2
+
+    .author
+      .author-name
+        font-weight: bold
 
 </style>
--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -6,9 +6,13 @@ const getIngredients = async (params: string | (string | null)[]) => {
 }
 
 const getRecipe = async (slug: string) => {
-  console.log()
   const res = await axios.get('http://localhost:3000/recipes/' + slug)
   return res.data
 }
 
-export default { getIngredients, getRecipe }
+const placeComment = async (slug: string, data: { text: string }, token: string) => {
+  const res = await axios.post('http://localhost:3000/recipes/' + slug + '/comment', data, { headers: { Authorization: `Bearer ${token}` } })
+  return res.data
+}
+
+export default { getIngredients, getRecipe, placeComment }
