3da9bc57cfad7f603a21b9ce8f0f18853b428f6c
Oscar Mollé <scarmoll@gmail.com>
2021-03-26 14:57:28 +0100

    homepage favorite recipes

--- src/services/PopularRecipesService.ts
+++ src/services/PopularRecipesService.ts
@@ -4,5 +4,10 @@ export default {
   async getPopularRecipes () {
     const res = await axios.get('http://localhost:3000/recipes/popular')
     return res.data
+  },
+
+  async getFavoriteRecipes (token: string) {
+    const res = await axios.get('http://localhost:3000/users/favorites', { headers: { Authorization: `Bearer ${token}` } })
+    return res.data
   }
 }
