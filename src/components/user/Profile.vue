251e27b30293a7b1cca2d986faa26b8fd659581e
A.R Leurs <59964230+leursa@users.noreply.github.com>
2021-06-02 12:46:42 +0200

    Added MyRecipes (#25)
    * Connection with endpoint and start with user profile page
    
    * Added functionality to userpage
    
    -Added the ablity for the user to update and delete their profile
    -Added some styling choices
    
    * myRecipes added to page
    
    * Overgezet naar nieuwe laptop
    
    Co-authored-by: StijnLimonard <70508897+StijnLimonard@users.noreply.github.com>

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -25,34 +25,60 @@
       <label>There is no description yet!</label>
       <md-input v-model="user.description" :readonly="!update"></md-input>
     </md-field>
-    <md-button class="md-raised" v-on:click="updateProfile" v-if="!update" >Change your information</md-button>
-    <md-button class="md-raised" v-on:click="updateProfile" v-if="update" >Cancel</md-button>
+    <md-button class="md-raised" v-on:click="updateProfile" v-if="!update">Change your information</md-button>
     <md-button class="md-raised" v-if="update" v-on:click="updateUser">Update your profile</md-button>
-      <md-dialog-confirm
-        :md-active.sync="active"
-        md-title="Delete your profile?"
-        md-content="Are you sure that you want to delete your profile?"
-        md-confirm-text="Yes"
-        md-cancel-text="No"
-        @md-confirm="deleteUser" />
-      <md-button class="md-raised" @click="active = true">Delete your profile</md-button>
+    <md-button class="md-raised" v-on:click="updateProfile" v-if="update">Cancel</md-button>
+    <md-dialog-confirm
+      :md-active.sync="active"
+      md-title="Delete your profile?"
+      md-content="Are you sure that you want to delete your profile?"
+      md-confirm-text="Yes"
+      md-cancel-text="No"
+      @md-confirm="deleteUser"/>
+    <md-button class="md-raised" @click="active = true" v-if="!update">Delete your profile</md-button>
+
+    <div v-if='loggedIn' class='container'>
+      <div>
+        <div class='md-headline'>Favorited by you</div>
+        <div class='block-layout' v-if="favoriteRecipes.length !== 0">
+          <Recipe class='recipe-box' v-for='recipe in favoriteRecipes' :key='recipe.id' :recipe='recipe.recipe'/>
+        </div>
+        <div class="block-layout" v-if="favoriteRecipes.length === 0">
+          There are no liked recipes yet!
+        </div>
+      </div>
+    </div>
+    <div v-if='loggedIn' class="container">
+      <div class="md-headline">My recipes</div>
+      <div class="block-layout">
+        <Recipe class="recipe-box" v-for="recipe in myRecipes" :key="recipe.id" :recipe="recipe"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import UserService from '@/services/UserService'
+import PopularRecipesService from '@/services/PopularRecipesService'
+import Recipe from '@/components/recipes/loop/recipe'
 
 export default {
   name: 'Profile',
+  components: { Recipe },
   data () {
     return {
+      favoriteRecipes: [],
+      myRecipes: [],
       user: Object,
       update: false,
-      active: false
+      active: false,
+      loggedIn: this.$store.getters.isLoggedIn
     }
   },
   created () {
     this.getUser()
+    this.getFavoriteRecipes()
+    this.getMyRecipes()
   },
   methods: {
     async getUser () {
@@ -87,11 +113,51 @@ export default {
               })
           }
         )
+    },
+
+    async getMyRecipes () {
+      if (!this.loggedIn) return
+
+      UserService.getMyRecipes(this.$store.state.token).then(
+        event => {
+          this.$set(this, 'myRecipes', event.recipes)
+        }
+      )
+    },
+
+    async getFavoriteRecipes () {
+      if (!this.loggedIn) return
+
+      PopularRecipesService.getFavoriteRecipes(this.$store.state.token)
+        .then(
+          event => {
+            this.$set(this, 'favoriteRecipes', event.recipes)
+          }
+        )
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang='sass' scoped>
+
+.container
+  background-color: #F6F4F5
+  width: 70%
+  padding: 15px
+  margin: auto auto 20px
+
+  .title
+    margin-bottom: 15px
+
+  .block-layout
+    display: flex
+    flex-direction: row
+
+    .recipe-box
+      width: 250px
+      height: 200px
+      overflow: hidden
+      margin-right: 15px
 
 </style>
