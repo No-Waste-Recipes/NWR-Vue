6dabb4e7909000c04faf6be65181e84d3d9244a2
A.R Leurs <59964230+leursa@users.noreply.github.com>
2021-06-07 16:55:45 +0200

    Added the recipe status
    Added recipe status and change the error handeling with the register

---
+++ src/components/recipes/loop/myrecipe.vue
@@ -0,0 +1,31 @@
+<template>
+  <RouterLink :to="{ name: 'RecipeDetail', params: { slug: recipe.slug }}">
+    <div class="recipe md-elevation-3">
+      <h2>{{ recipe.title }}</h2>
+      <h3 id="statusrecipe">{{ getStatus }}</h3>
+    </div>
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  name: 'Myrecipe',
+  props: {
+    recipe: {},
+    status: String
+  },
+  computed: {
+    getStatus () {
+      const firstLetter = this.recipe.status.charAt(0).toUpperCase()
+      return firstLetter + this.recipe.status.replaceAll('_', ' ').toLowerCase().slice(1)
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.recipe
+  background-color: #FFF
+  padding: 5px
+  text-align: left
+</style>
--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,54 +1,53 @@
 <template>
   <div>
     <h1>Profiel van {{ user.username }}</h1>
-    <md-field :md-clearable="update">
-      <label>Email</label>
-      <md-input v-model="user.email" :readonly="!update"></md-input>
-    </md-field>
-    <md-field :md-clearable="update">
-      <label>Username</label>
-      <md-input v-model="user.username" :readonly="!update"></md-input>
-    </md-field>
-    <md-field :md-clearable="update">
-      <label>First name</label>
-      <md-input v-model="user.first_name" :readonly="!update"></md-input>
-    </md-field>
-    <md-field :md-clearable="update">
-      <label>Last name</label>
-      <md-input v-model="user.last_name" :readonly="!update"></md-input>
-    </md-field>
-    <md-field :md-clearable="update" v-if="update">
-      <label>Password</label>
-      <md-input v-model="user.password"></md-input>
-    </md-field>
-    <md-field  v-if="update" :md-clearable="update">
-      <label>New password</label>
-      <md-input v-model="user.newPassword"></md-input>
-    </md-field>
-    <md-field :md-clearable="update" v-if="update">
-      <label>Repeat new password</label>
-      <md-input v-model="user.repeatPassword"></md-input>
-    </md-field>
-    <md-field v-if="user.description && user.description.length > 0" :md-clearable="update">
-      <label>Description</label>
-      <md-input v-model="user.description" :readonly="!update"></md-input>
-    </md-field>
-    <md-field v-else :md-clearable="update">
-      <label>There is no description yet!</label>
-      <md-input v-model="user.description" :readonly="!update"></md-input>
-    </md-field>
-    <md-button class="md-raised" v-on:click="updateProfile" v-if="!update">Change your information</md-button>
-    <md-button class="md-raised" v-if="update" v-on:click="updateUser">Update your profile</md-button>
-    <md-button class="md-raised" v-on:click="updateProfile" v-if="update">Cancel</md-button>
-    <md-dialog-confirm
-      :md-active.sync="active"
-      md-title="Delete your profile?"
-      md-content="Are you sure that you want to delete your profile?"
-      md-confirm-text="Yes"
-      md-cancel-text="No"
-      @md-confirm="deleteUser"/>
-    <md-button class="md-raised" @click="active = true" v-if="!update">Delete your profile</md-button>
-
+      <md-field :md-clearable="update">
+        <label>Email</label>
+        <md-input type="email" v-model="user.email" :readonly="!update"></md-input>
+      </md-field>
+      <md-field :md-clearable="update">
+        <label>Username</label>
+        <md-input v-model="user.username" :readonly="!update"></md-input>
+        </md-field>
+      <md-field :md-clearable="update">
+        <label>First name</label>
+        <md-input v-model="user.first_name" :readonly="!update"></md-input>
+        </md-field>
+      <md-field :md-clearable="update">
+        <label>Last name</label>
+        <md-input v-model="user.last_name" :readonly="!update"></md-input>
+        </md-field>
+      <md-field :md-clearable="update" v-if="update">
+        <label>Password</label>
+        <md-input type="password" v-model="user.password"></md-input>
+        </md-field>
+      <md-field :md-clearable="update" v-if="update">
+        <label>Repeat your password</label>
+        <md-input type="password" v-model="user.repeatPassword"></md-input>
+        </md-field>
+      <md-field v-if="update" :md-clearable="update">
+        <label>New password</label>
+        <md-input type="password" v-model="user.newPassword"></md-input>
+        </md-field>
+      <md-field v-if="user.description && user.description.length > 0" :md-clearable="update">
+        <label>Description</label>
+        <md-input v-model="user.description" :readonly="!update"></md-input>
+      </md-field>
+      <md-field v-else :md-clearable="update">
+        <label>There is no description yet!</label>
+        <md-input v-model="user.description" :readonly="!update"></md-input>
+      </md-field>
+      <md-button class="md-raised" v-on:click="updateProfile" v-if="!update">Change your information</md-button>
+      <md-button class="md-raised" type="submit" v-if="update" v-on:click="updateUser">Update your profile</md-button>
+      <md-button class="md-raised" v-on:click="updateProfile" v-if="update" >Cancel</md-button>
+      <md-dialog-confirm
+        :md-active.sync="active"
+        md-title="Delete your profile?"
+        md-content="Are you sure that you want to delete your profile?"
+        md-confirm-text="Yes"
+        md-cancel-text="No"
+        @md-confirm="deleteUser"/>
+      <md-button class="md-raised" @click="active = true" v-if="!update">Delete your profile</md-button>
     <div v-if='loggedIn' class='container'>
       <div>
         <div class='md-headline'>Favorited by you</div>
@@ -56,14 +55,17 @@
           <Recipe class='recipe-box' v-for='recipe in favoriteRecipes' :key='recipe.id' :recipe='recipe.recipe'/>
         </div>
         <div class="block-layout" v-if="favoriteRecipes.length === 0">
-          There are no liked recipes yet!
+          <p class="placeholder">There are no liked recipes yet!</p>
         </div>
       </div>
     </div>
     <div v-if='loggedIn' class="container">
       <div class="md-headline">My recipes</div>
       <div class="block-layout">
-        <Recipe class="recipe-box" v-for="recipe in myRecipes" :key="recipe.id" :recipe="recipe"/>
+        <Myrecipe class="recipe-box" v-for="recipe in myRecipes" :key="recipe.id" :recipe="recipe"/>
+      </div>
+      <div class="block-layout" v-if="myRecipes.length === 0">
+        <p class="placeholder">You haven't made any recipes yet!</p>
       </div>
     </div>
   </div>
@@ -73,10 +75,11 @@
 import UserService from '@/services/UserService'
 import PopularRecipesService from '@/services/PopularRecipesService'
 import Recipe from '@/components/recipes/loop/recipe'
+import Myrecipe from '@/components/recipes/loop/myrecipe'
 
 export default {
   name: 'Profile',
-  components: { Recipe },
+  components: { Myrecipe, Recipe },
   data () {
     return {
       favoriteRecipes: [],
@@ -154,7 +157,6 @@ export default {
 </script>
 
 <style lang='sass' scoped>
-
 .container
   background-color: #F6F4F5
   width: 70%
